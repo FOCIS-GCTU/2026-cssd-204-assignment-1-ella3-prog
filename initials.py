a95f823fb523ba2904ee617714e3ebc961f1e5cf
ella3-prog <emmanuellaopare970@gmail.com>
2026-05-31 23:07:21 +0000

    Assignment 2

--- initials.py
+++ initials.py
@@ -1,7 +1,36 @@
+# File: initials.py
+# Description: Print out my initials in large stylized letters.
+# Assignment Number: 1
+#
+# Name: Emmanuella Awukubea Opare
+# STUDENT ID: 2425404681
+# Email: 2425404681@live.gctu.edu.gh
+# Grader: Mr Augustus Buckman
+#
+# On my honor, Emmanuella Awukubea Opare, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+     # Print my initials in small and large stylized letters
 
+    print()
+
+    print("...EAO")
+    print()
+
+    print("...EEEEEEEEEEEE........AAAAAAAAAAAA........OOOOOOOOOOOO.....")
+    print("...E...................A..........A........O..........O.....")
+    print("...E...................A..........A........O..........O.....")
+    print("...EEEEEEEEEEEE........A..........A........O..........O.....")
+    print("...E...................AAAAAAAAAAAA........O..........O.....")
+    print("...E...................A..........A........O..........O.....")
+    print("...E...................A..........A........O..........O.....")
+    print("...E...................A..........A........O..........O.....")
+    print("...E..............**...A..........A...**...O..........O...**")
+    print("...EEEEEEEEEEEE...**...A..........A...**...OOOOOOOOOOOO...**")
+
+    print()
 
 
 main()
